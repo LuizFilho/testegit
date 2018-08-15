6f0e2cb8ab4afb1476258b87e4ecbfa1d312a548
LuizFilho <canaldoluiz66@gmail.com>
2018-08-14 22:07:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.get('/', (req,res) => res.status(200).send({message:'Olá Mundo, estou funcionando!!'}))
 
+app.get('/ola', (req,res) => res.status(200).send({message:'Oi , eu sou a rota Olá!'}))
+
 app.listen(3001, err => console.log('Api rodando na porta 3001'))
