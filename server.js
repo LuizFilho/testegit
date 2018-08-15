5f4e60deff39ed8768db74417a591a45ef3aed55
Luiz Filho <luizfilho.cont1@gmail.com>
2018-08-14 21:55:58 -0300

    Criado o Servidor api com Express

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => res.status(200).send({message:'Olá Mundo!'}))
+
+app.listen(3001, err => console.log('Api rodando na porta 3001'))
